c594cc704e52293c852ad1bdfd28809fd361366a
Nealle Page <nealle.page@gmail.com>
2023-05-27 22:54:07 +0100

    Update spacer.tsx

--- src/components/spacer.tsx
+++ src/components/spacer.tsx
@@ -9,7 +9,7 @@ export default function Example() {
       </div>
       <div className="relative flex justify-center">
         <span className="px-2 text-gray-500 bg-white">
-          <AcademicCapIcon className="w-5 h-5 text-gray-500" aria-hidden="true" />
+          <AcademicCapIcon className="w-5 h-5 text-olive-500" aria-hidden="true" />
         </span>
       </div>
     </div>
